61ac3249d25aef83758edb2372b00e8578591a7c
Tim Clifford <tim@worthlearning.co.uk>
2016-05-22 14:11:46 +0100

    please wait edits

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -253,17 +253,6 @@ jQuery(function($){
                 $(this).toggleClass('active');
             });
         }
-
-        // Please wait stuff
-        if ($(window).width() > 1024) {
-	        window.loading_screen = window.pleaseWait({
-	            logo: "assets/images/logoLarge.png",
-	            backgroundColor: '#222',
-	            loadingHtml: "<div class='spinner sk-spinner-wave'><div class='rect1'></div><div class='rect2'></div><div class='rect3'></div><div class='rect4'></div><div class='rect5'></div></div>"
-	        });
-	        window.loading_screen.finish();
-	    }
-
     };
 
     // Instantiate N10 prototype.
