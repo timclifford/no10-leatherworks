42f56426f5111bf287a2d472d64423e402f84745
Tim Clifford <tim@worthlearning.co.uk>
2016-05-22 14:43:27 +0100

    on load please wait splash

--- public/assets/js/please-wait.js
+++ public/assets/js/please-wait.js
@@ -1,14 +1,17 @@
 jQuery(function($){
     "use strict";    
 
-    // Please wait stuff
-    if ($(window).width() > 1024) {
-        window.loading_screen = window.pleaseWait({
-            logo: "assets/images/logoLarge.png",
-            backgroundColor: '#222',
-            loadingHtml: "<div class='spinner sk-spinner-wave'><div class='rect1'></div><div class='rect2'></div><div class='rect3'></div><div class='rect4'></div><div class='rect5'></div></div>"
-        });
-        window.loading_screen.finish();
-    }
+    $(window).on('load', function() {
+	    // Please wait stuff
+	    if ($(window).width() > 1024) {
+	        window.loading_screen = window.pleaseWait({
+	            logo: "assets/images/logoLarge.png",
+	            backgroundColor: '#222',
+	            loadingHtml: "<div class='spinner sk-spinner-wave'><div class='rect1'></div><div class='rect2'></div><div class='rect3'></div><div class='rect4'></div><div class='rect5'></div></div>"
+	        });
+	        window.loading_screen.finish();
+	    }
+
+	});
 
 });
